2b7040c4ce7937634c932cc868976d07f515a7c2
Pedro Henrique <pedro.henrique.torres@hotmail.com>
2022-02-07 19:55:48 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const generateScraperUrl = (apiKey) => `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Amazon Scrapper API.');
+})
+
+// GET Product Details
+app.get('/products/:productId', async (req, res) => {
+  const { productId } = req.params;
+  const { api_key } = req.query;
+
+  try {
+    const response = await axios.get(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/dp/${productId}`);
+
+    res.json(response.data);
+  } catch (error) {
+    res.json(error);    
+  }
+});
+
+// GET Product Reviews
+app.get('/products/:productId/reviews', async (req, res) => {
+  const { productId } = req.params;
+  const { api_key } = req.query;
+
+  try {
+    const response = await axios.get(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/product-reviews/${productId}`);
+
+    res.json(response.data);
+  } catch (error) {
+    res.json(error);    
+  }
+});
+
+// GET Product Offers
+app.get('/products/:productId/offers', async (req, res) => {
+  const { productId } = req.params;
+  const { api_key } = req.query;
+
+  try {
+    const response = await axios.get(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
+
+    res.json(response.data);
+  } catch (error) {
+    res.json(error);    
+  }
+});
+
+// GET Search Results
+app.get('/search/:searchQuery', async (req, res) => {
+  const { searchQuery } = req.params;
+  const { api_key } = req.query;
+
+  try {
+    const response = await axios.get(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/s?k=${searchQuery}`);
+
+    res.json(response.data);
+  } catch (error) {
+    res.json(error);    
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
